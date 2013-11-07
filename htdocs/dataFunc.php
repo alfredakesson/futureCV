1181a3996359f2e3d06876c78127cf3dc1907d7e
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-07 19:52:27 +0100

    hej

--- htdocs/dataFunc.php
+++ htdocs/dataFunc.php
@@ -2,4 +2,18 @@
 	function queryDB($query)
 	{
 		$handle = new SQLite3("data.db");
+		$array['dbhandle'] = $handle; 
+    	$array['query'] = $query;
+    	$result = $dbhandle->query($query);
+    	$i = 0; 
+    	while ($result->columnName($i)) 
+    	{ 
+        	$columns[ ] = $result->columnName($i); 
+        	$i++; 
+    	} 
+    
+    	$resx = $result->fetchArray(SQLITE3_ASSOC); 
+    	return $resx; 
 	}
+	echo "string";
+	echo queryDB("SELECT * FROM \"test\"");
