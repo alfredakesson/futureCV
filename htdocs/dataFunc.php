5367613e876f40d23d5f99d75dbb04aa17c32cc5
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-07 19:33:22 +0100

    add the files

---
+++ htdocs/dataFunc.php
@@ -0,0 +1,5 @@
+<?php
+	function queryDB($query)
+	{
+		$handle = new SQLite3("data.db");
+	}
---
+++ htdocs/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="top_border">
+			<ul>
+   				<li><a href='#'>Generate CV</a></li>
+   				<li><a href='#'>Preview CV</a></li>
+			</ul>
+		</div>
+		
+
+
+		<div class="container_box"><h2>Jens Gustafsson</h2>
+			<div id="community_container">
+				<div class="small_square">
+					<p>F</p>
+				</div>
+
+				<div class="small_square_img">
+					<img src="facebook.png" width="25px">
+				</div>				
+
+			</div>
+
+
+		</div>
+
+
+		
+
+
+
+	</body>
+</html>
