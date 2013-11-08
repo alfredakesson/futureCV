48974f555c9b959fff34515f151bbbc9e4ce9ca4
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-08 13:36:19 +0100

    test

--- htdocs/generator_cp.php
+++ htdocs/generator_cp.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
@@ -16,7 +15,7 @@
 			<h1 style="margin-top: 30px; margin-bottom:50px">Step 1: <font color="#808080">Choose a layout!</font></h1>
 			<div id="container_box_mini">
 
-				<a href=#form_box>
+				<a href='#'>
 					<div class="box_mini">
 						<div class="square"></div>
 						<div class="square" style="background-color:red"></div>
@@ -25,7 +24,7 @@
 					</div>
 				</a>
 				
-				<a href=#form_box>
+				<a href='#'>
 					<div class="box_mini">
 						<div class="rectangle_horizontal" style="background-color:red"></div>
 						<div class="rectangle_horizontal" style="background-color:blue">
@@ -34,7 +33,7 @@
 						<div class="rectangle_horizontal" style="background-color:green"></div>
 					</div>
 				</a>
-				<a href=#form_box>
+				<a href='#'>
 					<div class="box_mini">
 						<div class="rectangle_vertical" style="background-color:red"></div>
 						<div class="rectangle_vertical" style="background-color:blue">
@@ -58,27 +57,25 @@
 					</div>
 					
 					<div class="form_div" style= "background-color:green;">
-<<<<<<< HEAD
-						<p>Skriv text:</p>
-						<textarea rows="4" cols="50" name="text1"></textarea>
-=======
 						
 						<textarea rows="4" cols="50" name="text1">
 						</textarea>
-						<p></p>
->>>>>>> df9703a56514b2da4adecce829de6256b8abd35a
 
+						<p>Välj bakgrundsfärg:</p>
+						<select name="color1">
+  							<option>Azure</option>
+  							<option>Coral</option>
+  							<option>Cyan</option>
+  							<option>Gold</option>
+  							<option>Green</option>
+  							<option>Indigo</option>
 						</select>
-						<div class="color_box" style="background-color:green"></div>
-						<div class="color_box" style="background-color:yellow;"></div>
-						<div class="color_box" style="background-color:red;"></div>
-						<div class="color_box" style="background-color:blue;"></div>
 					</div>
 				
 					<div class="form_div" style= "background-color:yellow;">
-<<<<<<< HEAD
 						<p>Skriv text:</p>
-						<textarea rows="4" cols="50" name="text2"></textarea>
+						<textarea rows="4" cols="50" name="text2">
+						</textarea>
 						<p>Välj bakgrundsfärg:</p>
 						<select name="color2">
   							<option>Azure</option>
@@ -87,25 +84,13 @@
   							<option>Gold</option>
   							<option>Green</option>
   							<option>Indigo</option>
-=======
-						
-						<textarea rows="4" cols="50" name="text2">
-						</textarea>
-						<p></p>
-
-
->>>>>>> df9703a56514b2da4adecce829de6256b8abd35a
 						</select>
-						<div class="color_box" style="background-color:green"></div>
-						<div class="color_box" style="background-color:yellow;"></div>
-						<div class="color_box" style="background-color:red;"></div>
-						<div class="color_box" style="background-color:blue;"></div>
 					</div>
 
 					<div class="form_div" style= "background-color:red;">
-<<<<<<< HEAD
 						<p>Skriv text:</p>
-						<textarea rows="4" cols="50" name="text3"></textarea>
+						<textarea rows="4" cols="50" name="text3">
+						</textarea>
 						<p>Välj bakgrundsfärg:</p>
 						<select name="color3">
   							<option>Azure</option>
@@ -119,7 +104,8 @@
 	
 					<div class="form_div" style= "background-color:blue;">
 						<p>Skriv text:</p>
-						<textarea rows="4" cols="50" name="text4"></textarea>
+						<textarea rows="4" cols="50" name="text4">
+						</textarea>
 						<p>Välj bakgrundsfärg:</p>
 						<select name="color4">
   							<option>Azure</option>
@@ -130,33 +116,8 @@
   							<option>Indigo</option>
 						</select>
 						<input type="submit" value="Skicka">
-=======
-						
-						<textarea rows="4" cols="50" name="text3">
-						</textarea>
-						<p></p>
-
-						<div class="color_box" style="background-color:green"></div>
-						<div class="color_box" style="background-color:yellow;"></div>
-						<div class="color_box" style="background-color:red;"></div>
-						<div class="color_box" style="background-color:blue;"></div>
-					</div>
-	
-					<div class="form_div" style= "background-color:blue;">
-						
-						<textarea rows="4" cols="50" name="text4">
-						</textarea>
-						<p></p>
-
-						<p></p>
-						<div class="color_box" style="background-color:green"></div>
-						<div class="color_box" style="background-color:yellow;"></div>
-						<div class="color_box" style="background-color:red;"></div>
-						<div class="color_box" style="background-color:blue;"></div>	
->>>>>>> df9703a56514b2da4adecce829de6256b8abd35a
 					</div>
 				</form>
-				<div style="text-align:center; margin-top:15px"><input type="submit" value="Generate CV"></div>
 			</div>
 		</div>
 		
