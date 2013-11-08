d22428866cb2947ed5595ecaea3889e7698434b2
jensgustafsson <jensgustafsson@live.com>
2013-11-08 08:44:13 +0100

    skapade en generator.php fil

---
+++ htdocs/generator.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="top_border">
+			<ul>
+   				<li><a href='#'>Generate CV</a></li>
+   				<li><a href='#'>Preview CV</a></li>
+			</ul>
+		</div>
+		
+
+
+		<div class="container_box"><h2>Jens Gustafsson</h2>
+			<div id="community_container">
+				<!--<div class="small_square">
+					<p>F</p>
+				</div>-->
+
+				<div class="small_square_img">
+					<img src="facebook.png" width="25px">
+				</div>				
+				<div class="small_square_img">
+					<img src="email.png" width="25px">
+				</div>	
+
+				<div class="small_square_img">
+					<img src="linkedin.png" width="25px">
+				</div>	
+
+			</div>
+
+
+
+<!--
+Arbetslivserfarenhet
+Egenskaper
+Utbildning
+Top5-"VALFRITT"  <<Programmeringsspråk
+-->
+
+		</div>
+
+
+		
+
+
+
+	</body>
+</html>
