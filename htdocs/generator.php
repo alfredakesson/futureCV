87e9bd64513ff725db98e2caa37cef43ab4f940b
jensgustafsson <jensgustafsson@live.com>
2013-11-08 11:05:41 +0100

    viktigt commitmeddelande

--- htdocs/generator.php
+++ htdocs/generator.php
@@ -12,7 +12,7 @@
 		</div>
 		
 		<div id="content">
-			<h1>Step 1: <font color="#808080">Choose a layout!</font></h1>
+			<h1 style="margin-top: 30px; margin-bottom:50px">Step 1: <font color="#808080">Choose a layout!</font></h1>
 			<div id="container_box_mini">
 
 				<a href='#'>
@@ -45,9 +45,73 @@
 
 			</div>
 			
+			<h1 style="margin-top: 0px; margin-bottom:50px;">Step 2: <font color="#808080">Fill your boxes!</font></h1>
 
+			<div id="form_box">
+				
 
+					<form name="input" action="cratePage.php" method="post">
+				<div class="form_div" style= "background-color:blue;">
+					<p>Skriv Namn:</p>
+					<input type="text" name="name">
+				</div>
+				<div class="form_div" style= "background-color:green;">
+						<p>Skriv text:</p>
+						<textarea rows="4" cols="50" name="text1">
+						</textarea>
+					</div>
+						<p>Välj bakgrundsfärg:</p>
+						<select name="color1">
+  							<option>Azure</option>
+  							<option>Coral</option>
+  							<option>Cyan</option>
+  							<option>Gold</option>
+  							<option>Green</option>
+  							<option>Indigo</option>
+						</select>
 
+					<p>Skriv text:</p>
+					<textarea rows="4" cols="50" name="text2">
+					</textarea>
+					<p>Välj bakgrundsfärg:</p>
+					<select name="color2">
+  						<option>Azure</option>
+  						<option>Coral</option>
+  						<option>Cyan</option>
+  						<option>Gold</option>
+  						<option>Green</option>
+  						<option>Indigo</option>
+					</select>
+
+					<p>Skriv text:</p>
+					<textarea rows="4" cols="50" name="text3">
+					</textarea>
+					<p>Välj bakgrundsfärg:</p>
+					<select name="color3">
+  						<option>Azure</option>
+  						<option>Coral</option>
+  						<option>Cyan</option>
+  						<option>Gold</option>
+  						<option>Green</option>
+  						<option>Indigo</option>
+					</select>
+
+					<p>Skriv text:</p>
+					<textarea rows="4" cols="50" name="text4">
+					</textarea>
+					<p>Välj bakgrundsfärg:</p>
+					<select name="color4">
+  						<option>Azure</option>
+  						<option>Coral</option>
+  						<option>Cyan</option>
+  						<option>Gold</option>
+  						<option>Green</option>
+  						<option>Indigo</option>
+					</select>
+
+					<input type="submit" value="Skicka">
+				</form>
+			</div>
 		</div>
 		
 
