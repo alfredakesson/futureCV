c0c10499afb1bb43963b111c1fd7b76e3017aacc
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-09 12:10:57 +0100

    a change

--- htdocs/cratePage.php
+++ htdocs/cratePage.php
@@ -1,5 +1,5 @@
 <?php
-$file = file_get_contents(" .html");
+$file = file_get_contents("tempEqSizeBox.html");
 $file = str_replace("%%name%%",$_POST["name"],$file);
 $file = str_replace("%%text1%%",$_POST["text1"],$file);
 $file = str_replace("%%text2%%",$_POST["text2"],$file);
@@ -12,6 +12,9 @@ $file = str_replace("%%color4%%",$_POST["color4"],$file);
 $length = 5;
 $randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 $filename = $randomString.".html";
-file_put_contents($filename,$file);
-header("Location: /$filename");
-exit;
+if (false) {
+	file_put_contents($filename,$file);
+	header("Location: /$filename");
+	exit;
+}
+echo $file;
