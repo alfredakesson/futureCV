760f1c9cc48b3857677869c71bb1991da0493cc7
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-09 16:21:27 +0100

    php funk

--- htdocs/php_fnc.php
+++ htdocs/php_fnc.php
@@ -20,21 +20,9 @@
 	}
 
 if(isset($_FILES["file"])){
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 20000)
-&& in_array($extension, $allowedExts))
-  {
   if ($_FILES["file"]["error"] > 0)
     {
-    //echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     }
   else
     {
@@ -54,10 +42,6 @@ if ((($_FILES["file"]["type"] == "image/gif")
       //echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
       }
     }
-  }
-else
-  {
-  //echo "Invalid file";
   }
 }
 ?>
